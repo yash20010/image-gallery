3b2c7613171735d911a4d26f28fbc7cfbd09338e
throne.yash@gmail.com <throne.yash@gmail.com>
2021-09-21 15:44:31 -0600

    Adding pixabay url to the images for the user to navigate over

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -5,7 +5,9 @@ const ImageCard = ({ image }) => {
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src={image.webformatURL} alt="" />
+      <a href={image.pageURL} target="_blank" rel="noreferrer">
+        <img src={image.webformatURL} alt="" />
+      </a>
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl mb-2">
           Photo by {image.user}
